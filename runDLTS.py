5fd840904e11482d670541d9fff21a642d874f48
pekermilas <46932127+pekermilas@users.noreply.github.com>
2026-06-16 08:50:33 -0400

    Added data leveling check to run library.
    Added data leveling check to run library. This is for the sanity of the data. If there is a increased noise or drift, data will be able to catch it by itself.

--- runDLTS.py
+++ runDLTS.py
@@ -40,108 +40,18 @@ if __name__ == '__main__':
     # data = iaT.impdData()
     # data.readData()
     # fN = 'C:/Users/peker/Documents/GitHub/PekerPersonalCodeWorks/CemilDataAnalysis/06092026/-1p0.txt'
-    # fN = 'C:/Users/peker/Documents/GitHub/PekerPersonalCodeWorks/CemilDataAnalysis/06092026/-72p0.txt'
+    fN = 'C:/Users/peker/Documents/GitHub/PekerPersonalCodeWorks/CemilDataAnalysis/06092026/-72p0.txt'
     # fN = 'C:/Users/pekermilas/Documents/GitHub/PekerPersonalCodeWorks/CemilDataAnalysis/06092026/-1p0.txt'
-    fN = 'C:/Users/pekermilas/Documents/GitHub/PekerPersonalCodeWorks/CemilDataAnalysis/06092026/-90p0.txt'
+    # fN = 'C:/Users/pekermilas/Documents/GitHub/PekerPersonalCodeWorks/CemilDataAnalysis/06092026/-90p0.txt'
     # fN = 'C:/Users/pekermilas/Documents/GitHub/PekerPersonalCodeWorks/CemilDataAnalysis/06092026/-72p0.txt'
     data = iaT.impdData(fName=fN)
     data.readData()
 
-    m, c, l = data.findDataLevelsScikitLearn()
-    plt.scatter(data.dataValues[list(data.dataValues)[0]]['timeStampImps'],
-                # data.dataValues[list(data.dataValues)[0]]['ImpedanceIm'],c=l[idx],
-                data.dataValues[list(data.dataValues)[0]]['ImpedanceIm'], c=l[:],
-                cmap='coolwarm', s=4)
-    x = np.array(data.dataValues[list(data.dataValues)[0]]['timeStampImps'])
-    y = np.array(data.dataValues[list(data.dataValues)[0]]['ImpedanceIm'])
+    run = rdT.dltsRun()
+    run.testDataLeveling(fN, plot=True)
 
-    # Data sanity check by evolution of standard deviation
-    #------------------------------------------------------
-    for j in range(len(l[0])):
-        if j == 0:
-            idx = [0]
-            val = [l[0, 0]]
-        else:
-            if not l[0, j] == val[-1]:
-                idx.append(j)
-                val.append(l[0, j])
-    diffs = []
-    pairs = []
-    for j in range(len(val)):
-        if (val[j] == 1) and (j + 1 < len(idx)):
-            diffs.append(idx[j + 1] - idx[j])
-            pairs.append([idx[j], idx[j + 1]])
 
-    commonLength = statistics.mode(np.array(diffs)[np.array(diffs) > 1])
-    idx = np.array(pairs)[np.where(np.array(diffs) == commonLength)[0]]
-    if len(idx) > 0:
-        if len(idx) == 1:
-            yy = np.array(y[idx[0]:idx[1]])
-            xx = np.array(x[idx[0]:idx[1]])
-            xx = xx - xx[0]
-        if len(idx) > 1:
-            yy = np.array(y[idx[0][0]:idx[0][1]])
-            xx = np.array(x[idx[0][0]:idx[0][1]])
-            xx = xx - xx[0]
-            for k in range(1, len(idx)):
-                temp = np.array(y[idx[k][0]:idx[k][1]])
-                yy = np.column_stack((yy, temp))
-                temp = np.array(x[idx[k][0]:idx[k][1]])
-                temp = temp - temp[0]
-                xx = np.column_stack((xx, temp))
-    std0 = np.mean(np.std(yy,axis=0))
-    std1 = np.std(yy)
-
-    # Data sanity check by fitting
-    # ------------------------------------------------------
-    xx = x[l[0]==1]
-    yy = y[l[0] == 1]
-
-    xx = xx / xx[0]
-    yy = yy / yy[0]
-    # Fit linear model using lmfit
-    from lmfit.models import LinearModel, PolynomialModel
-    
-    # Linear fit
-    model_linear = LinearModel()
-    params_linear = model_linear.guess(yy, x=xx)
-    result_linear = model_linear.fit(yy, params_linear, x=xx)
-
-    # Cubic polynomial fit
-    model_cubic = PolynomialModel(degree=3)
-    params_cubic = model_cubic.guess(yy, x=xx)
-    result_cubic = model_cubic.fit(yy, params_cubic, x=xx)
-
-    # Report linear fit results
-    print("\n=== Linear Fit Results ===")
-    print(f"Slope: {result_linear.params['slope'].value:.6e} ± {result_linear.params['slope'].stderr:.6e}")
-    print(f"Intercept: {result_linear.params['intercept'].value:.6e} ± {result_linear.params['intercept'].stderr:.6e}")
-    print(f"R-squared: {result_linear.rsquared}")
-    print(f"Chi-squared: {result_linear.chisqr}")
-    print(f"Reduced chi-squared: {result_linear.redchi}")
-
-    # Report cubic fit results
-    print("\n=== Cubic Polynomial Fit Results ===")
-    print(f"c3 (x^3 coefficient): {result_cubic.params['c3'].value:.6e} ± {result_cubic.params['c3'].stderr:.6e}")
-    print(f"c2 (x^2 coefficient): {result_cubic.params['c2'].value:.6e} ± {result_cubic.params['c2'].stderr:.6e}")
-    print(f"c1 (x coefficient): {result_cubic.params['c1'].value:.6e} ± {result_cubic.params['c1'].stderr:.6e}")
-    print(f"c0 (constant): {result_cubic.params['c0'].value:.6e} ± {result_cubic.params['c0'].stderr:.6e}")
-    print(f"R-squared: {result_cubic.rsquared}")
-    print(f"Chi-squared: {result_cubic.chisqr}")
-    print(f"Reduced chi-squared: {result_cubic.redchi}")
-    
-    # Plot both fits overlayed with data
-    plt.figure(figsize=(10, 6))
-    plt.scatter(xx, yy, label='Data', s=4, alpha=0.6, color='black')
-    yfit_linear = result_linear.best_fit
-    yfit_cubic = result_cubic.best_fit
-    plt.plot(xx, yfit_linear, 'r-', linewidth=2, label=f'Linear Fit (R²={result_linear.rsquared:.4f})')
-    plt.plot(xx, yfit_cubic, 'b-', linewidth=2, label=f'Cubic Fit (R²={result_cubic.rsquared:.4f})')
-    plt.xlabel('Normalized Time Stamp')
-    plt.ylabel('Normalized Impedance')
-    plt.legend()
-    plt.grid(True, alpha=0.3)
-    plt.show()
+    # xx = data.dataValues['tickStampImps']
     
     # plt.plot(xx, yy, '.', markersize=2)
     #
